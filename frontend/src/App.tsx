fa67a68968e4432d3b6bbb6637be1fcb6fe26766
kepelivi <kepelivi7@gmail.com>
2024-11-08 17:58:23 +0100

    Implement routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import Posts from "./components/Posts"
+import Posts from "./components/Posts";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Posts />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
