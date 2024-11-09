36b0fb0d5ab81b7cbacdceab01e41e45f19b4940
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-11-09 19:45:03 +0100

    Navbar component created

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <>
             <h1>Home page</h1>
             <p>Welcome to our page! Check out our posts and users:</p>
-            <ul>
+            <ul className="navbar">
                 <li><Link to={'/posts'}>Posts</Link></li>
                 <li><Link to={'/users'}>Users</Link></li>
             </ul>
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar : React.FC = ()=>{
+    return(<ul className="navbar">
+        <li><Link to={'/posts'}>Posts</Link></li>
+        <li><Link to={'/users'}>Users</Link></li>
+    </ul>)
+}
+export default Navbar;
