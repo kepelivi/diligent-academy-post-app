de139a519353b4fc23db14dd8c0f6155aa7cc0eb
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-11-09 19:53:46 +0100

    Navbar styled

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <>
-            <h1>Home page</h1>
-            <p>Welcome to our page! Check out our posts and users:</p>
             <ul className="navbar">
                 <li><Link to={'/posts'}>Posts</Link></li>
                 <li><Link to={'/users'}>Users</Link></li>
             </ul>
+            <h1>Home page</h1>
+            <p>Welcome to our page! Check out our posts and users:</p>
+            
            
         </>
     )
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar : React.FC = ()=>{
     return(<ul className="navbar">
+        <li><Link to={'/'}>Home</Link></li>
         <li><Link to={'/posts'}>Posts</Link></li>
         <li><Link to={'/users'}>Users</Link></li>
     </ul>)
