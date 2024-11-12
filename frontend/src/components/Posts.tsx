4270d710630677a964f9d820636a333bb7b4b3c4
kepelivi <kepelivi7@gmail.com>
2024-11-12 17:17:22 +0100

    Refactor Posts with searching integrated

--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import PostItem from "./PostItem";
 import Navbar from "./NavBar";
+import SearchForPost from "./SearchForPosts";
 
 export interface Post {
     id: number;
@@ -14,9 +15,12 @@ export default function Posts() {
     const queryClient = useQueryClient();
     const [title, setTitle] = useState<string>('');
     const [content, setContent] = useState<string>('');
+    const [author, setAuthor] = useState<string>('');
+    const [findBy, setFindBy] = useState<"author" | "content" | "title">("author");
+    const [textToFind,setTextToFind] = useState<string>('');
 
-    const { data: posts, isError, isPending, error } = useQuery<Post[]>({
-        queryFn: async () => fetch('http://localhost:3000/posts').then(res => res.json()),
+    const { data: posts, isError, isPending, error, refetch } = useQuery<Post[]>({
+        queryFn: async () => fetch(textToFind != '' ? `http://localhost:3000/posts?${findBy}=${textToFind}` :'http://localhost:3000/posts').then(res => res.json()),
         queryKey: ['posts']
     })
 
@@ -50,7 +54,7 @@ export default function Posts() {
             id: getHeighestPossibleID(),
             title: title,
             content: content,
-            author: "Universal User"
+            author: author,
         })
         console.log(posts);
     }
@@ -64,10 +68,12 @@ export default function Posts() {
     return (
         <>
             <Navbar />
+            <SearchForPost posts={posts} setFindBy={setFindBy} textToFind={textToFind} setTextToFind={setTextToFind} refetch={refetch}/>
             <div className="input-container">
                 <h3>Add a new post</h3>
                 <input type="text" placeholder="Title" name="title" onChange={(e) => setTitle(e.target.value)} /> <br></br>
                 <textarea placeholder="Post content" name="content" onChange={(e) => setContent(e.target.value)} /> <br></br>
+                <input type="text" placeholder="Author" name="author" onChange={(e) => setAuthor(e.target.value)} /> <br></br>
                 <button onClick={handleCreatePost}>Create post</button>
             </div>
             <ul className="post-list">
