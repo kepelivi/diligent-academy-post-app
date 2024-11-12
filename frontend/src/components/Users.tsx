7e621948c7b4bd0c5c5ea83323c5546efab86f4d
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-11-12 17:53:52 +0100

    Bugfix: add user is now rerenders the app

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -67,7 +67,7 @@ export default function Users() {
 
   const createUserMutation = useMutation({
     mutationFn: async (newUser: Omit<User, "id">) => {
-      fetch("http://localhost:3000/users", {
+      await fetch("http://localhost:3000/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: newUser.name, email: newUser.email }),
